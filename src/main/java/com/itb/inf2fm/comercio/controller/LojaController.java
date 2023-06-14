4132b22652fdaec36965c9e74db3f26a0739576d
dudaacarvalho <eduarda.mecap@gmail.com>
2023-06-14 08:18:52 -0300

    Edição da LojaController

---
+++ src/main/java/com/itb/inf2fm/comercio/controller/LojaController.java
@@ -0,0 +1,10 @@
+package com.itb.inf2fm.comercio.controller;
+
+public class LojaController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/itb/inf2fm/comercio/model/Produto.java
@@ -0,0 +1,10 @@
+package com.itb.inf2fm.comercio.model;
+
+public class Produto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
